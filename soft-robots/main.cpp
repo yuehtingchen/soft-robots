24cc8802643e63f7ab7bf232673d1a655ad594ac
yuehtingchen <alicetingchen@gmail.com>
2022-10-21 16:57:45 -0400

    Initial Commit

---
+++ soft-robots/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  soft-robots
+//
+//  Created by CJChen on 2022/10/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
